364e89dc3cf2656cf7b317973ab41a30c5bc5c1a
John Briggs <jbriggs@greenixpc.com>
2025-08-05 07:51:54 -0600

    Initial Commit

---
+++ PestGenieTests/PestGenieTests.swift
@@ -0,0 +1,17 @@
+//
+//  PestGenieTests.swift
+//  PestGenieTests
+//
+//  Created by John Briggs on 8/5/25.
+//
+
+import Testing
+@testable import PestGenie
+
+struct PestGenieTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
